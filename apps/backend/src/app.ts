18015d9defbfdae1c1687c2315fdd0a34fce2447
Z3AMA <zedohany@outlook.com>
2025-11-21 23:10:13 +0200

    feat: add passport session and field component export

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,10 +1,30 @@
-import express, { Express } from "express";
+import express from "express";
 import cors from "cors";
+import passport from "passport";
+import session from "express-session";
+
+import { setupPassport } from "./config/passport.config.js";
 import authRoutes from "./routes/auth.routes.js";
 
-export const app: Express = express();
+export const app = express(); 
 
-app.use(cors());
 app.use(express.json());
+app.use(cors({ origin: "*" }));
+
+// Required for passport
+app.use(
+  session({
+    secret: "SECRET_SESSION",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+setupPassport();
 
 app.use("/auth", authRoutes);
+
+app.listen(4000, () => console.log("Server Running"));
